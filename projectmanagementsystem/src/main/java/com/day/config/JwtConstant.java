94a5edf2e70407ba965de43e292ab74e4f10df87
Jangchaeyun <zzangsally@naver.com>
2024-08-31 12:35:05 +0900

    0831 Modal

---
+++ projectmanagementsystem/src/main/java/com/day/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.day.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "scertjwripofjkcrew09jfoicnlksmdp9w2eujroiewnlkewffcertht";
+    public static final String JWT_HEADER = "Authorization";
+}
---
+++ projectmanagementsystem/src/main/java/com/day/modal/Chat.java
@@ -0,0 +1,19 @@
+package com.day.modal;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Chat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private Project project;
+}
