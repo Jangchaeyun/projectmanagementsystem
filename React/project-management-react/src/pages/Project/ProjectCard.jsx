281f8a426badddeae19995c060ceabc8b91cdc85
Jangchaeyun <zzangsally@naver.com>
2024-09-03 21:36:44 +0900

    0903 ProjectCard & ProjectList

---
+++ React/project-management-react/src/pages/Project/ProjectCard.jsx
@@ -0,0 +1,56 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import {
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
+import { DotFilledIcon, DotsVerticalIcon } from "@radix-ui/react-icons";
+
+const ProjectCard = () => {
+  return (
+    <Card className="p-5 w-full lg:max-w-3xl">
+      <div className="space-y-5">
+        <div className="space-y-2">
+          <div className="flex justify-between">
+            <div className="flex items-center gap-5">
+              <h1 className="cursor-pointer font-bold text-lg">
+                Create Project Management System
+              </h1>
+              <DotFilledIcon />
+              <p className="text-sm text-gray-400">fullstack</p>
+            </div>
+            <div>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Button className="rounded-full" variant="ghost" size="icon">
+                    <DotsVerticalIcon />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem>수정</DropdownMenuItem>
+                  <DropdownMenuItem>삭제</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </div>
+          <p className="text-gray-500 text-sm">
+            Project Management System은 프로젝트를 팀원들을 초대하고 관리하는
+            시스템을 말한다.
+          </p>
+        </div>
+        <div className="flex flex-wrap gap-2 items-center">
+          {[1, 1, 1, 1].map((item) => (
+            <Badge key={item} variant="outline">
+              {"frontend"}
+            </Badge>
+          ))}
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default ProjectCard;
