b0d9305c8ba66f5b18edeb7010d9e5bec9d7a389
Jangchaeyun <zzangsally@naver.com>
2024-09-08 20:02:47 +0900

    0908 Subscription Redux

---
+++ React/project-management-react/src/Redux/Subscription/Reducer.js
@@ -0,0 +1,53 @@
+const { error } = require("console");
+const {
+  GET_USER_SUBSCRIPTION_REQUEST,
+  UPGRADE_SUBSCRIPTION_REQUEST,
+  GET_USER_SUBSCRIPTION_SUCCESS,
+  UPGRADE_SUBSCRIPTION_SUCCESS,
+  GET_USER_SUBSCRIPTION_FAILURE,
+  UPGRADE_SUBSCRIPTION_FAILURE,
+} = require("./ActionTypes");
+
+const initialState = {
+  userSubscription: null,
+  loading: false,
+  error: null,
+};
+
+const subscriptionReducer = (state = initialState, action) => {
+  switch (action.payload) {
+    case GET_USER_SUBSCRIPTION_REQUEST:
+    case UPGRADE_SUBSCRIPTION_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+
+    case GET_USER_SUBSCRIPTION_SUCCESS:
+      return {
+        ...state,
+        userSubscription: action.payload,
+        loading: false,
+        error: null,
+      };
+    case UPGRADE_SUBSCRIPTION_SUCCESS:
+      return {
+        ...state,
+        userSubscription: action.payload,
+        loading: false,
+        error: null,
+      };
+    case GET_USER_SUBSCRIPTION_FAILURE:
+    case UPGRADE_SUBSCRIPTION_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default subscriptionReducer;
